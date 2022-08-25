2d475e2ebf810125dfbae26edde8bac31b730895
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-25 14:06:22 -0700

    added last fetch routes

--- fetch-utils.js
+++ fetch-utils.js
@@ -54,6 +54,21 @@ export async function getUser() {
     }
 }
 
+export async function getTasks() {
+    const res = await fetch(`${BASE_URL}/api/v1/tasks`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+    });
+    if (res.ok) {
+        const tasks = await res.json();
+        return tasks;
+    }
+}
+
 export async function redirectIfLoggedIn() {
     const user = await getUser();
     if (user) {
@@ -74,4 +89,51 @@ export async function logout() {
     if (res.ok) {
         location.replace('../');
     }
+}
+
+export async function addTask(task) {
+    const res = await fetch(`${BASE_URL}/api/v1/tasks`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task),
+        credentials: 'include'
+    }
+    );
+    const data = await res.json();
+    return data;
+}
+
+export async function deleteTask(id) {
+    const res = await fetch(`${BASE_URL}/api/v1/tasks/${id}`, {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+    });
+    const data = await res.json();
+    return data;
+}
+
+export async function updateTask(id) {
+    const res = await fetch(`${BASE_URL}/api/v1/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify({ completed: true }),
+    });
+    const data = await res.json();
+    if (res.ok) {
+        return data;
+    } else {
+        throw new Error(data.message);
+    }
+    
 }
--- tasks/tasks.js
+++ tasks/tasks.js
@@ -1,9 +1,36 @@
-import { checkUser, logout, } from '../fetch-utils.js';
+import { addTask, checkUser, getTasks, logout, } from '../fetch-utils.js';
+import { renderTasks } from '../render-utils.js';
 const logoutBtn = document.getElementById('logout');
+const taskInput = document.getElementById('add-task');
+
 
 checkUser();
 
 logoutBtn.addEventListener('click', async () => {
     await logout();
 });
+
+taskInput.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(taskInput);
+    await addTask({
+        taskName: formData.get('task-name'),
+        description: formData.get('task-description'),
+    });
+    location.replace('/');
+});
+
+export async function displayTasks() {
+    const taskDisplay = document.getElementById('task-display');
+
+    const tasks = await getTasks();
+
+    for (let task of tasks) {
+        const taskOutput = renderTasks(task);
+        taskDisplay.append(taskOutput);
+    }
+}
+
+displayTasks();
+
 
